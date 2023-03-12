6c19193829778895191295a5ee777f7590a62570
franrolg <francisco.roldang0@gmail.com>
2023-03-12 05:58:55 -0300

    fix: simplificar imports del archivo producto

--- producto.py
+++ producto.py
@@ -1,5 +1,4 @@
-from generador import generar_id, color, warning, success
-
+import generador as gen
 from proveedor import Proveedor, proveedores
 
 class Producto(): # definición de la clase y sus atributos.
@@ -13,7 +12,7 @@ class Producto(): # definición de la clase y sus atributos.
     color: str # éste es el nuevo atributo.
 
     def __init__(self, nombre, categoria, proveedor, stock, valor_neto, color): # constructor de la clase.
-        self.sku = str(generar_id(10))
+        self.sku = str(gen.generar_id(10))
         self.nombre = nombre.title()
         self.categoria = categoria.title()
         self.proveedor = proveedor
@@ -39,8 +38,8 @@ productos = [producto_1, producto_2, producto_3, producto_4, producto_5]
 def menu_productos():
     while True:
         
-        print(success("Menú Productos:"))
-        opcion = input(f'{color("1)")} Productos\n{warning("0)")} Regresar\n>> ')
+        print(gen.success("Menú Productos:"))
+        opcion = input(f'{gen.color("1)")} Productos\n{gen.warning("0)")} Regresar\n>> ')
         if opcion == '0':
             break
 
@@ -49,4 +48,4 @@ def menu_productos():
                 print(producto.mostrar_productos())
 
         else:
-            print(warning('Ingresar una opción válida.'))
+            print(gen.warning('Ingresar una opción válida.'))
